20299a0830296b86c626f51f776c7eaba66dd9cf
Marco van Est <marcovanest@me.com>
2014-06-11 11:31:15 +0200

    PSR-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,13 +10,4 @@ define("NS", "\\");
 define("ROOT_DIR", $_SERVER['DOCUMENT_ROOT']);
 define("LOG_DIR", '/vagrant/var/www/log');
 
-require_once("../library/Centralino/Core/Autoloader.php");
-
-require_once('../vendor/autoload.php');
-
-$autoload = new PSR4Autoloader();
-$autoload->register();
-$autoload->addNamespace('Centralino', '/vagrant/var/www/benny/library/Centralino');
-$autoload->addNamespace('Centralino', '/vagrant/var/www/benny/tests/Centralino');
-$autoload->addNamespace('Tests', '/vagrant/var/www/benny/tests');
-
+require_once(__DIR__.'/../../../autoload.php');
