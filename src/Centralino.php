616b9ae16b501a5f0c604013c72f2206e23f74d8
Marco van Est <marcovanest@me.com>
2014-06-11 12:10:49 +0200

    Added Centralino class

---
+++ src/Centralino.php
@@ -0,0 +1,18 @@
+<?php
+namespace Centralino;
+
+class Centralino
+{
+    private $container;
+
+    public function __construct()
+    {
+
+    }
+
+    public function initialize()
+    {
+        $this->container['logger'] = new Centralino\Core\Logger\DefaultLogger(LOG_DIR . DS . 'tests', 'database.log');
+
+    }
+}
