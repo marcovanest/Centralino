1cde64f650b6b8c7d141f5096d6ca8b6298fb3cb
Marco van Est <marcovanest@me.com>
2014-07-09 16:07:52 +0200

    Added new test

--- tests/Centralino/Database/PDO/Statement/ExecuteTest.php
+++ tests/Centralino/Database/PDO/Statement/ExecuteTest.php
@@ -30,6 +30,33 @@ class ExecuteTest extends \PHPUnit_Framework_TestCase
         $stm->execute();
     }
 
+    /**
+     * @expectedException Centralino\Database\DatabaseException
+     * @expectedMessage Statement failed to execute
+     */
+    public function testExecute_Returns_False_Throws()
+    {
+        $stmtMock = $this->getMockBuilder('\PDOStatement')
+                    ->getMock();
+
+        $stmtMock->expects($this->any())
+                    ->method('execute')
+                    ->will($this->returnValue(false));
+
+        $pdoMock = $this->getMockBuilder('\Centralino\Database\PDO\_files\PDOMock')
+                    ->setMethods(array('prepare'))
+                    ->getMock();
+
+        $pdoMock->expects($this->any())
+                    ->method('prepare')
+                    ->will($this->returnValue($stmtMock));
+
+        $manager = new \Centralino\Database\PDO\Manager($pdoMock);
+
+        $stm = $manager->select('SELECT * FROM LOG');
+        $stm->execute();
+    }
+
     public function testExecute_Succes_Returns_Centralino_PDOStatement()
     {
         $stmtMock = $this->getMockBuilder('\PDOStatement')
