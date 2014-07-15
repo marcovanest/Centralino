702178296867575f5f9b6d7685fb4a76687a15e7
Marco van Est <marcovanest@me.com>
2014-07-15 21:18:25 +0200

    First Router and Services implementation

--- tests/Centralino/Database/PDO/PDOConnectionTest.php
+++ tests/Centralino/Database/PDO/PDOConnectionTest.php
@@ -5,16 +5,20 @@ class PDOConnectionTest extends \PHPUnit_Framework_TestCase
 {
     public function testIs_Initializable()
     {
-        $driver     = new Driver\Pgsql('localhost', null, 'benny_test', 'postgres', 'tar');
-        $connection = new PDOConnection($driver);
+        $driverStub = $this->getMockBuilder('Centralino\Database\PDO\Driver\Pgsql')
+                        ->getMock();
+
+        $connection = new PDOConnection($driverStub);
 
         $this->assertInstanceOf('Centralino\Database\PDO\PDOConnection', $connection);
     }
 
     public function testCreate()
     {
-        $driver     = new Driver\Pgsql('localhost', null, 'benny_test', 'postgres', 'tar');
-        $connection = new PDOConnection($driver);
+        $driverStub = $this->getMockBuilder('Centralino\Database\PDO\Driver\Pgsql')
+                        ->getMock();
+
+        $connection = new PDOConnection($driverStub);
 
         $this->assertInstanceOf('\PDO', $connection->create());
     }
